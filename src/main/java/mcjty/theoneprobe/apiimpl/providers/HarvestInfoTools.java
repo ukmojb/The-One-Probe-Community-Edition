aac0cb3ba2a9290ac22a69013956964667a625b0
xuanyu tang <3465854398@qq.com>
2023-04-15 21:44:31 +0800

    支持矿石阶段

--- src/main/java/mcjty/theoneprobe/apiimpl/providers/HarvestInfoTools.java
+++ src/main/java/mcjty/theoneprobe/apiimpl/providers/HarvestInfoTools.java
@@ -7,6 +7,9 @@ import mcjty.theoneprobe.api.ILayoutStyle;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.config.Config;
 import mcjty.theoneprobe.items.ModItems;
+import net.darkhax.bookshelf.util.PlayerUtils;
+import net.darkhax.gamestages.GameStageHelper;
+import net.darkhax.orestages.api.OreTiersAPI;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
@@ -17,6 +20,7 @@ import net.minecraft.item.ItemTool;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.util.Tuple;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
@@ -76,8 +80,20 @@ public class HarvestInfoTools {
     static void showHarvestInfo(IProbeInfo probeInfo, World world, BlockPos pos, Block block, IBlockState blockState, EntityPlayer player) {
         boolean harvestable = block.canHarvestBlock(world, pos, player) && world.getBlockState(pos).getBlockHardness(world, pos) >= 0;
 
+        Tuple<String, IBlockState> stageInfo = OreTiersAPI.getStageInfo(blockState);
+        if (stageInfo != null && !GameStageHelper.clientHasStage(PlayerUtils.getClientPlayer(), stageInfo.getFirst())) {
+            Block stageBlock = stageInfo.getSecond().getBlock();
+            if (stageBlock.canHarvestBlock(world, pos, player) && world.getBlockState(pos).getBlockHardness(world, pos) >= 0) {
+                harvestable = true;
+            } else {
+                harvestable = false;
+            }
+        }
+
         String harvestTool = block.getHarvestTool(blockState);
         String harvestName = null;
+
+
 
         if (harvestTool == null) {
             // The block doesn't have an explicitly-set harvest tool, so we're going to test our wooden tools against the block.
@@ -100,15 +116,30 @@ public class HarvestInfoTools {
             }
         }
 
+
         if (harvestTool != null) {
+            if (stageInfo != null && !GameStageHelper.clientHasStage(PlayerUtils.getClientPlayer(), stageInfo.getFirst())) {
+                IBlockState stageBlockState = stageInfo.getSecond();
+                Block stageBlock = stageInfo.getSecond().getBlock();
+                harvestTool = stageBlock.getHarvestTool(stageBlockState);
+            }
+
             int harvestLevel = block.getHarvestLevel(blockState);
+
             if (harvestLevel < 0) {
                 // NOTE: When a block doesn't have an explicitly-set harvest tool, getHarvestLevel will return -1 for ANY tool. (Expected behavior)
 //                TheOneProbe.logger.info("HarvestLevel out of bounds (less than 0). Found " + harvestLevel);
             } else if (harvestLevel >= harvestLevels.length) {
 //                TheOneProbe.logger.info("HarvestLevel out of bounds (Max value " + harvestLevels.length + "). Found " + harvestLevel);
             } else {
-                harvestName = harvestLevels[harvestLevel];
+                if (stageInfo != null && !GameStageHelper.clientHasStage(PlayerUtils.getClientPlayer(), stageInfo.getFirst())) {
+                    IBlockState stageBlockState = stageInfo.getSecond();
+                    Block stageBlock = stageInfo.getSecond().getBlock();
+                    int stageharvestLevel = stageBlock.getHarvestLevel(stageBlockState);
+                    harvestName = harvestLevels[stageharvestLevel];
+                } else {
+                    harvestName = harvestLevels[harvestLevel];
+                }
             }
             harvestTool = StringUtils.capitalize(harvestTool);
         }
@@ -129,10 +160,10 @@ public class HarvestInfoTools {
                 else
                     ToolClassString = harvestTool.substring(0, 1).toUpperCase() + harvestTool.substring(1);
 
-                horizontal.icon(ICONS, 16, offs, dim, dim, iconStyle)
+                horizontal.icon(ICONS, 0, offs, dim, dim, iconStyle)
                         .text(OK + ToolClassString + " (" + I18n.format("top.level") + " " + harvestName + ")");
             } else {
-                horizontal.icon(ICONS, 16, offs, dim, dim, iconStyle)
+                horizontal.icon(ICONS, 0, offs, dim, dim, iconStyle)
                         .text(OK + I18n.format("top.NoTool") + " (" + I18n.format("top.level") + " " + harvestName + ")");
             }
 
@@ -143,7 +174,9 @@ public class HarvestInfoTools {
             } else {
                 if (harvestTool != null) {
                     String ToolClassString;
+                    if (stageInfo != null && !GameStageHelper.clientHasStage(PlayerUtils.getClientPlayer(), stageInfo.getFirst())) {
 
+                    }
                     if (I18n.hasKey("top.toolclass." + harvestTool))
                         ToolClassString = I18n.format("top.toolclass." + harvestTool);
                     else
@@ -158,4 +191,5 @@ public class HarvestInfoTools {
             }
         }
     }
+
 }
